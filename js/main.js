d63117dd0a3929edf0e500b73bf0c7f1f7e62c64
Alice <alice.mouchard85@gmail.com>
2018-09-29 16:06:12 +0200

    Modif exercice

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+// Ajouter les stations sur  
+var map;
+var mapLat = 47.216694;
+var mapLng = -1.553809;
+var mapDefaultZoom = 12;
+
+// On initialise la map
+function initialize_map() {
+	map = new ol.Map({
+	 	target: "map",
+	 	layers: [new ol.layer.Tile({source: new ol.source.OSM({url: "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"})})],
+	 	view: new ol.View({center: ol.proj.fromLonLat([mapLng, mapLat]),
+	 										 zoom: mapDefaultZoom})
+ 	});
+ }
+
+ function add_map_point(lat, lng) {
+ 	var vectorLayer = new ol.layer.Vector({source:new ol.source.Vector({
+																						features: [new ol.Feature({
+																							geometry: new ol.geom.Point(ol.proj.transform([parseFloat(lng), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857'))
+																						})]
+																					}),
+ 																				style: new ol.style.Style({
+ 																					image: new ol.style.Icon({anchor: [0.5, 0.5],
+ 																																		anchorXUnits: "fraction",
+ 																																		anchorYUnits: "fraction",
+ 																																		src: "https://upload.wikimedia.org/wikipedia/commons/e/ec/RedDot.svg"
+ 																																	})
+ 																																})
+ 																				});
+
+	map.addLayer(vectorLayer);
+}
+
+initialize_map();
+
 fetch('http://api.citybik.es/v2/networks/bicloo').then(function(res) {
 	return res.json().then(function(data) {
 		console.log(data);
@@ -6,10 +42,15 @@ fetch('http://api.citybik.es/v2/networks/bicloo').then(function(res) {
     var stations = data.network.stations;
 
     for (var i = 0; i < stations.length; i++) {
+
+			add_map_point(stations[i].latitude, stations[i].longitude);
+
 			// ajout li pour chaque station : nom  + places libres + nb velos
     	var currentLi = document.createElement('li');
+			var currentSpan = document.createElement('span');
       var detailStation = stations[i].name + ' Places libres : ' + stations[i].empty_slots + ' Vélos disponibles : ' + stations[i].free_bikes;
-      currentLi.textContent = detailStation;
+      currentSpan.textContent = detailStation;
+			currentLi.appendChild(currentSpan);
       listeStations.appendChild(currentLi);
 
 			// code couleur en fonction du nombre de velo dispo
@@ -17,26 +58,26 @@ fetch('http://api.citybik.es/v2/networks/bicloo').then(function(res) {
 			var currentDiv = document.createElement('div');
 			currentLi.appendChild(currentDiv);
 
-			if (total_velo === stations[i].free_bikes) {
+			if (total_velo === stations[i].free_bikes){
 				//console.log('complet');
 				var newClassComplet = document.createAttribute("class");
 				newClassComplet.value = "complet";
 				currentDiv.setAttributeNode(newClassComplet);
 				//currentDiv.textContent = 'Tous les vélos sont dispo!';
-
-			} else if (stations[i].free_bikes > total_velo / 2) {
+			}
+			else if (stations[i].free_bikes >= total_velo / 2){
 				//console.log('plus de la moitie');
 				var newClassPlus = document.createAttribute("class");
 				newClassPlus.value = "plus_moitie";
 				currentDiv.setAttributeNode(newClassPlus);
-
-			} else if (stations[i].free_bikes < total_velo / 2) {
+			}
+			else if (stations[i].free_bikes < total_velo / 2){
 				//console.log('moins de la moitie');
 				var newClassMoins = document.createAttribute("class");
 				newClassMoins.value = "moins_moitie";
 				currentDiv.setAttributeNode(newClassMoins);
-
-			} else if (total_velo === stations[i].empty_slots) {
+			}
+			else if (total_velo === stations[i].empty_slots){
 				//console.log('vide');
 				var newClassVide = document.createAttribute("class");
 				newClassVide.value = "vide";
@@ -44,8 +85,6 @@ fetch('http://api.citybik.es/v2/networks/bicloo').then(function(res) {
 			}
 
     }
-
-
 
 	});
 });
